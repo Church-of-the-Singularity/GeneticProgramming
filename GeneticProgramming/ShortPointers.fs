2b039e1e672327d86a087fa97621a412d3640d06
Victor Milovanov <lostfreeman@gmail.com>
2018-06-24 16:16:02 -0700

    implemented untested garbage collection in the pool

--- GeneticProgramming/ShortPointers.fs
+++ GeneticProgramming/ShortPointers.fs
@@ -1,10 +1,18 @@
 ﻿module ShortPointers
 
+open System.Reflection
+open System.Collections.Generic
+
+type Bits = System.Collections.BitArray
+
 type IPointer<'P> =
     abstract member Address: 'P
 
 type ITypedPointer<'P, 'T when 'T: struct> =
     inherit IPointer<'P>
+
+type IManaged<'P, 'T when 'T: struct> =
+  abstract member GetReferences: unit -> seq<ITypedPointer<'P, 'T>>
 
 [<Struct>]
 type TypedPointer<'P, 'T when 'T: struct>(ptr: 'P) =
@@ -24,13 +32,15 @@ type StructPool<'P, 'T when 'T: struct> =
 
 let inline makePool<'P, 'T when 'T: struct> size =
     { data = Array.zeroCreate<'T> size;
-      allocated = System.Collections.BitArray(size);
+      allocated = Bits(size);
       phantom = phantom<'P> }
+
+let inline Ref (pool: StructPool<'P,'T>) (pointer: ITypedPointer<'P, 'T>) = &pool.data.[int pointer.Address]
 
 let inline New< ^P, 'T
             when ^P : (static member op_Explicit: int -> ^P)
              and ^P : equality // WTF?
-             and 'T: struct > (pool: StructPool< ^P, 'T >) (value: 'T) =
+             and 'T : struct > (pool: StructPool< ^P, 'T >) (value: 'T) =
     let mutable result = None
     for start = 0 to pool.allocated.Length - 1 do
         if result <> None then ()
@@ -42,13 +52,43 @@ let inline New< ^P, 'T
             result <- Some(TypedPointer< ^P, 'T >(ptr))
     result
 
+let inline GcNew< ^P, 'T
+            when ^P : (static member op_Explicit: int -> ^P)
+             and ^P: (static member op_Explicit: ^P -> int)
+             and ^P : equality // WTF?
+             and 'T :> IManaged< ^P, 'T>
+             and 'T: struct > (pool: StructPool< ^P, 'T >) (value: 'T)
+                              (roots: seq<ITypedPointer< ^P, 'T>>) =
+  let collectGarbage() =
+    let alive = Bits(pool.data.Length)
+    let visited = Bits(pool.data.Length)
+    let visitQueue = Queue()
+    for root in roots do
+      visitQueue.Enqueue(root)
+      alive.[int root.Address] <- true
+    let wasVisited(address: ^P) = int address < pool.data.Length && visited.[int address]
+    while visitQueue.Count > 0 do
+      let aliveObject = visitQueue.Dequeue()
+      if not(wasVisited aliveObject.Address) then
+        let obj = Ref pool aliveObject
+        for reference in obj.GetReferences() do
+          visitQueue.Enqueue(reference)
+    for i = 0 to pool.allocated.Length - 1 do
+      pool.allocated.[i] <- alive.[i]
+
+  match New pool value with
+  | Some p -> Some(p)
+  | None ->
+    collectGarbage()
+    New pool value
+
 let inline Release< ^P, 'T when 'T : struct and 'T : unmanaged 
                             and ^P: (static member op_Explicit: ^P -> int) > (pool: StructPool<'P,'T>) (pointer: TypedPointer<'P, 'T>) =
     if pool.allocated.[int pointer.Address] then
         pool.allocated.[int pointer.Address] <- false
     else
         invalidOp "double deallocation"
-let inline Ref (pool: StructPool<'P,'T>) (pointer: TypedPointer<'P, 'T>) = &pool.data.[int pointer.Address]
+
 
 [<Struct>]
 type BytePtr =
