0ffe997984227efdd8aef57bf53123557ad3dfcc
Victor Milovanov <lostfreeman@gmail.com>
2018-07-28 12:52:41 -0700

    added a stub for ShortDataModel

---
+++ GeneticProgramming/ShortDataModel.fs
@@ -0,0 +1,11 @@
+﻿module GeneticProgramming.ShortDataModel
+
+module ShortList =
+  open Operators
+
+  let empty pool = notImplemented()
+  let cons pool head tail = notImplemented()
+  let switch pool list onEmpty onPopulated = notImplemented()
+  let length pool list = notImplemented()
+
+
