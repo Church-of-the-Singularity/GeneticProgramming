174b90b58f6603f8612355c9b62ce532c37a73da
Victor Milovanov <lostfreeman@gmail.com>
2018-08-01 13:08:01 -0700

    implemented ShortDataModel (not tested)

--- GeneticProgramming/IDataModel.fs
+++ GeneticProgramming/IDataModel.fs
@@ -2,13 +2,13 @@
 
 type IDataModel =
     abstract Convert: value:obj -> obj
-    abstract ConvertBack: value:obj -> obj
+    abstract ConvertBack: value:obj * targetType:System.Type -> obj
 
 type FSharpDataModel private () =
     static let instance = FSharpDataModel()
 
     interface IDataModel with
         member this.Convert o = id o
-        member this.ConvertBack o = id o
+        member this.ConvertBack(o, _) = id o
 
     static member Instance = instance
--- GeneticProgramming/ShortDataModel.fs
+++ GeneticProgramming/ShortDataModel.fs
@@ -9,8 +9,8 @@ module ShortList =
 
   let private oom() = raise(System.OutOfMemoryException())
 
-  let inline empty<'T when 'T: struct and 'T:> System.IConvertible>
-      (pool: IPool<Ptr<'T>, 'T>)
+  let inline empty
+      (pool: StructPool<_,_>)
       gc = genericNull<Ptr<'T>, 'T>
 
   let rec cons pool gc head tail =
@@ -45,6 +45,78 @@ module ShortList =
       result <- result + 1
       current := tail pool !current
     result
+
+open System.Reflection
+open Lost.Pointers
+
+type private GenericListHelper<'T> private () =
+  static member Empty = List.empty<'T>
+  static member Cons(head:'T, tail) = List.Cons(head, tail)
+  static member Rev(list: list<'T>) = List.rev list
+
+type ShortDataModel<'W when 'W: struct and 'W :> System.IConvertible>(pool: StructPool.StructPool<Ptr<'W>, 'W>, gc) =
+    let cons = ShortList.cons pool gc
+    let empty: TypedPointer<Ptr<'W>, 'W> = ShortList.empty pool gc
+
+    member this.Convert(value: int): 'W =
+      let ptr = Ptr<'W>.op_Explicit value
+      Pointers.getAddress ptr
+    
+    member this.Convert(value: int list): 'W =
+      List.foldBack
+        (fun item tail ->
+          let convertedItem = this.Convert(item: int)
+          cons convertedItem (getAddress tail))
+        value
+        empty
+      |> getAddress |> getAddress
+
+    member this.Convert(o: obj): 'W =
+        match o with
+        | :? int as i -> this.Convert i
+        | :? list<int> as simpleList -> this.Convert simpleList
+        | :? System.Collections.IEnumerable as nested ->
+          Seq.foldBack
+            (fun (item: obj) tail ->
+                let convertedItem = this.Convert item
+                ShortList.cons pool gc convertedItem (getAddress tail))
+            (Seq.cast<obj> nested)
+            (ShortList.empty pool gc)
+          |> getAddress |> getAddress
+        | _ -> invalidArg "o" "unsupported type"
+
+    member this.ConvertBack(value: 'W, targetType): obj =
+        if targetType = typeof<int> then
+          box(value.ToInt32 null)
+        elif targetType = typeof<list<int>> then
+          let list = TypedPointer<Ptr<'W>, 'W>(Ptr<'W>.op_Explicit(value))
+          let rec toReversed list =
+            ShortList.switch pool list
+              (fun () -> [])
+              (fun head tail -> head.ToInt32(null) :: toReversed tail)
+          let result: int list = toReversed list |> List.rev
+          box result
+        else
+          let list = TypedPointer<Ptr<'W>, 'W>(Ptr<'W>.op_Explicit(value))
+          let itemType = targetType.GetGenericArguments().[0]
+          let helper = typedefof<GenericListHelper<int list>>.MakeGenericType(itemType)
+          let empty = helper.GetProperty("Empty", BindingFlags.Static ||| BindingFlags.Public).GetValue(null)
+          let cons = helper.GetMethod("Cons", BindingFlags.Static ||| BindingFlags.Public)
+          let rec toReversed list =
+            ShortList.switch pool list
+              (fun () -> empty)
+              (fun head tail ->
+                cons.Invoke(null, [|
+                  this.ConvertBack(head, itemType),
+                  this.ConvertBack(getAddress(getAddress(tail)), targetType)|]))
+          let reversed = toReversed list
+          helper.GetMethod("Rev", BindingFlags.Static ||| BindingFlags.Public)
+                .Invoke(null, [|reversed|])
+
+    interface GeneticProgramming.IDataModel with
+        member this.Convert o = box <| this.Convert o
+        member this.ConvertBack(o, targetType) = this.ConvertBack(o :?> 'W, targetType)
+    
 
 
 
